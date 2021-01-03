b39522f639fc5fd4a27976ca41dc2afa717cf2d4
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-03 18:10:43 -0300

    nova pasta

---
+++ conceitos-basicos/variaveis-tipos_de_dados.js
@@ -0,0 +1,30 @@
+var nome = "Diego Rodrigues"; //aspas duplas
+
+var sobrenome = 'Oliveira'; // aspas simples
+
+nome = "Diego" // o 'var' somente é utilizado para declarar, para alterar não precisa.
+
+
+// TIPOS DE DADOS:
+var nomeCompleto = "Diego Rodrigues de Oliveira"; //String
+
+var idade = 30; // Inteiro
+
+var altura = 1.73; // Float 
+
+var santista = true // Booleano
+
+var familia = ["Maria", "Ademir", "Andreia", "Joelma"] // Vetor (Array)
+
+var sobrinho = {
+    nome: "Drin",
+    altura: 1.75,
+    peso: 60.5,
+    santista: false
+}; // Objeto
+
+
+console.log(familia)
+console.log(familia[3])
+console.log(familia[0])
+console.log(sobrinho.peso)
