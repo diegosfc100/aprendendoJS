eefa65bf38274f2ab49f43841aa8b96d53bf4101
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-04 11:05:24 -0300

    add operadores relacionais

---
+++ conceitos-basicos/operadores-relacionais.js
@@ -0,0 +1,23 @@
+/*
+
+igual a ==
+diferente de !=
+maior q >
+menor q <
+maior ou igual a >= 
+menor ou igual a <=
+igual e do mesmo tipo q ===
+diferente e do mesmo tipo q !==
+
+*/
+
+var n1 = 99, n2 = 1;
+
+console.log(n1 == n2);
+console.log(n1 != n2);
+console.log(n1 > n2);
+console.log(n1 < n2);
+console.log(n1 >= n2);
+console.log(n1 <= n2);
+console.log(n1 === n2);
+console.log(n1 !== n2);
