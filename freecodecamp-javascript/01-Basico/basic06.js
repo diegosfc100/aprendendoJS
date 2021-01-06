9f9561cd8646da242a6c6114f09f1b21363eb625
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-06 10:38:57 -0300

    exercicio adicionado

---
+++ freecodecamp-javascript/01-Basico/basic06.js
@@ -0,0 +1,14 @@
+/* Compreendendo variáveis não inicializadas.
+
+Quando as variáveis JavaScript são declaradas, elas têm um valor inicial de indefinido.
+Se você fizer uma operação matemática em uma variável indefinida, seu resultado será NaN
+que significa "Não é um número". Se você concatenar uma string com uma variável indefinida,
+obterá uma string literal de "undefined".
+
+Inicialize as três variáveis a, b e c com 5, 10 e "Eu sou o", respectivamente, para que não sejam indefinidas. 
+
+EXERCÍCIO RESOLVIDO ABAIXO: */ 
+
+var a = 5;
+var b = 10;
+var c = "I am a";
