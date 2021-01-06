37e98941b8f978863ec8b9e4685c345751ada70c
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-06 11:35:12 -0300

    exercício 13 adicionado em 01-Basico

---
+++ freecodecamp-javascript/01-Basico/basic14.js
@@ -0,0 +1,15 @@
+/* Crie números decimais com JavaScript
+
+Podemos armazenar números decimais em variáveis também.
+Os números decimais às vezes são chamados de números de ponto flutuante ou Float.
+
+Nota:
+Nem todos os números reais podem ser representados com precisão em ponto flutuante. 
+Isso pode levar a erros de arredondamento.
+
+Crie uma variável myDecimal e atribua a ela um valor decimal com uma parte fracionária
+(por exemplo, 5,7).
+
+EXERCÍCIO RESOLVIDO ABAIXO: */ 
+
+var myDecimal = 5.7;
