4b325e72e5fe336286d6559d219fd4e47dc43c48
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-07 08:51:11 -0300

    exercício 28 adicionado em 01-Basico

---
+++ freecodecamp-javascript/01-Basico/basic28.js
@@ -0,0 +1,18 @@
+/* Construindo Strings com Variáveis
+
+Às vezes, você precisará construir uma string, no estilo Mad Libs. Usando o operador de concatenação (+),
+você pode inserir uma ou mais variáveis em uma string que está construindo.
+
+Exemplo: */
+
+var ourName = "freeCodeCamp";
+var ourStr = "Olá, nosso nome é" + ourName + ", como você está?";
+// ourStr agora é "Olá, nosso nome é freeCodeCamp, como vai você?" 
+
+/* Defina myName como uma string igual ao seu nome e construa myStr com myName entre as strings
+"My name is " and " and I am well!"
+
+EXERCÍCIO RESOLVIDO ABAIXO: */ 
+
+var myName = "Diego"
+var myStr = "My name is " + myName + " and I am well!"
