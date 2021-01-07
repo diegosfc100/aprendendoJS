0b3f1afe1b5c13852edcc1c3f2a76c1ab98f3635
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-07 08:51:24 -0300

    exercício 29 adicionado em 01-Basico

---
+++ freecodecamp-javascript/01-Basico/basic29.js
@@ -0,0 +1,20 @@
+/* Anexando Variáveis a Strings
+
+Assim como podemos construir uma string em várias linhas de literais de string,
+também podemos anexar variáveis a uma string usando o operador mais igual (+=).
+
+Exemplo: */
+
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is";
+ourStr += anAdjetivo;
+// ourStr agora é "freeCodeCamp is awesome!"
+
+/* Defina someAdjective como uma string de pelo menos 3 caracteres e anexe-a a myStr usando o operador +=.
+
+EXERCÍCIO RESOLVIDO ABAIXO: */ 
+
+
+var someAdjective = "Awesome"
+var myStr = "Learning to code is ";
+myStr += someAdjective;
