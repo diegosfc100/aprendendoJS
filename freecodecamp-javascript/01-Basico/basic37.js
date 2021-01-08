cefceadb28408e0e990730cf40c896d9a80bebf6
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-08 11:59:13 -0300

    exercício 37 adicionado em 01-Básico

---
+++ freecodecamp-javascript/01-Basico/basic37.js
@@ -0,0 +1,13 @@
+/* Armazene vários valores em uma variável usando Arrays
+
+Com as variáveis de array JavaScript, podemos armazenar vários dados em um só lugar.
+Você inicia uma declaração de matriz com um colchete de abertura, termina com um colchete de fechamento
+e coloca uma vírgula entre cada entrada, assim:
+
+var sanduíche = ["manteiga de amendoim", "geléia", "pão"].
+
+Modifique a nova matriz myArray para que contenha uma string e um número (nessa ordem).
+
+EXERCÍCIO RESOLVIDO ABAIXO: */ 
+
+var myArray = ["SEVEN", 7]
