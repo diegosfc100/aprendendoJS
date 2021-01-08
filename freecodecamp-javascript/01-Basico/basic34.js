a73adc7edcfa94367e329fb27cab4eea7d62014d
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-08 11:56:37 -0300

    exercício 34 adicionado em 01-Básico

---
+++ freecodecamp-javascript/01-Basico/basic34.js
@@ -0,0 +1,22 @@
+/* Use a notação de colchetes para encontrar o último caractere em uma string
+
+Para obter a última letra de uma string, você pode subtrair uma do comprimento da string.
+
+Por exemplo: */
+var firstName = "Charles"
+/* você pode obter o valor da última letra da string usando firstName [firstName.length - 1].
+
+Exemplo: */
+
+var firstName = "Charles";
+var lastLetter = firstName[firstName.length - 1]; // lastLetter é "s"
+
+/* Use a notação de colchetes para localizar o último caractere na variável lastName.
+
+EXERCÍCIO RESOLVIDO ABAIXO: */ 
+
+
+var lastName = "Lovelace"
+
+var lastLetterOfLastName = lastName[lastName.length - 1]
+
