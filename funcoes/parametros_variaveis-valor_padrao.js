51f87230f523087a75009b9f081972fbc1528640
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-13 07:20:26 -0300

    conteudo revisado

--- funcoes/parametros_variaveis-valor_padrao.js
+++ funcoes/parametros_variaveis-valor_padrao.js
@@ -32,74 +32,34 @@ imprime_valores(3, 4, 5);
 imprime_valores(6, 7, 8, 9, 10);
 console.log("========================")
 
-// Gambiarra 1
 
-function somar2(num1, num2, num3){
-    num1 = num1 || 1;
-    num2 = num2 || 2;
-    num3 = num3 || 3;
-
-    return num1 + num2 + num3;
-}
-
-//console.log(somar2(4, 5, 6)); // 15
-//console.log(somar2()); // 6
-//console.log(somar2(2)); // 7
-//console.log(somar2(2, 2)); // 7
-//console.log(somar2(2, 2, 2)); // 6
-//console.log(somar2(2, 2, 2, 2)); // 6
-
-//console.log(somar2(0, 0, 0)); // era pra ser 0, mas ele retorna 6
-
-// Gambiarra 2
-
-function somar3(num1, num2, num3){
-    num1 = isNaN(num1) ? 1 : num1;
-    num2 = isNaN(num2) ? 2 : num2;
-    num3 = isNaN(num3) ? 3 : num3;
-
-    return num1 + num2 + num3;
-}
-
-// console.log(somar3(4, 5, 6)); // 15
-// console.log(somar3()); // 6
-// console.log(somar3(2)); // 7
-// console.log(somar3(2, 2)); // 7
-// console.log(somar3(2, 2, 2)); // 6
-// console.log(somar3(2, 2, 2, 2)); // 6
-
-// console.log(somar3(0, 0, 0)); // 0
-
-
-// Forma atual - recomendada
+// VALOR PADRÃO - Forma atual (recomendada)
 function somar4(num1 = 1, num2 = 2, num3 = 3){
     return num1 + num2 + num3;
 }
 
-// console.log(somar4(4, 5, 6)); // 15
-// console.log(somar4()); // 6
-// console.log(somar4(2)); // 7
-// console.log(somar4(2, 2)); // 7
-// console.log(somar4(2, 2, 2)); // 6
-// console.log(somar4(2, 2, 2, 2)); // 6
-// console.log(somar4(0, 0, 0)); // 0
+console.log(somar4(4, 5, 6)); 
+console.log(somar4()); 
+console.log(somar4(2)); 
+console.log(somar4(2, 2)); 
+console.log(somar4(2, 2, 2)); 
+console.log(somar4(2, 2, 2, 2)); 
+console.log(somar4(0, 0, 0));
+console.log("========================")
 
-//console.log(somar4('a', 'b', 'c')); // 'abc'
-//console.log(somar4(true, false, 'c')); // '1c'
-//console.log(somar4(2, true, false)); // 3
+console.log(somar4('a', 'b', 'c')); 
+console.log(somar4(true, false, 'c')); 
+console.log(somar4(2, true, false)); 
+console.log("========================")
 
-/*
- Em JavaScript, 1 é considerado true e 0 é considerado false
-*/
-
-
-/*
+//
 
 function somar5(num1 = 1, num2 = 2, num3 = 3){
-    if(Number.isSafeInteger(num1) && Number.isSafeInteger(num2) && Number.isSafeInteger(num3)){
+    if(Number.isSafeInteger(num1) && Number.isSafeInteger(num2) && Number.isSafeInteger(num3)){ 
+        // se os valores forem numeros faça a soma, se não retorne que a mensagem não foi possivel.
         return num1 + num2 + num3;
     }else{
-        return -1;
+        return "Não foi possível efetuar a soma.";
     }
 }
 
@@ -111,10 +71,6 @@ console.log(somar5(2, 2)); // 7
 console.log(somar5(2, 2, 2)); // 6
 console.log(somar5(2, 2, 2, 2)); // 6
 console.log(somar5(0, 0, 0)); // 0
-
 console.log(somar5('a', 'b', 'c')); // 'abc'
 console.log(somar5(true, false, 'c')); // '1c'
 console.log(somar5(2, true, false)); // 3
-
-
-*/
