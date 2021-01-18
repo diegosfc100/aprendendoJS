7269b4a1ae808342cdd02198c706a43ef171018c
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-18 09:09:13 -0300

    add orientação a objetos

---
+++ orientacao-a-objeto/class-atributos.js
@@ -0,0 +1,14 @@
+class Pessoa { // classes sempre com letra maiuscula.
+    constructor(nome, sobrenome){ // metodo: constructor ; atributos: nome, sobrenome
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+class Carro {
+    constructor(modelo){
+        this.marca = 'Honda';
+        this.modelo = modelo;
+    }
+}
+
