c6a3a97a7f2529a8c335b01d69c03733f1702609
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-19 11:14:16 -0300

    add Alterando Estilos

---
+++ manipulando-DOM/alterando-estilos.js
@@ -0,0 +1,26 @@
+let caixaElement = document.querySelector('.caixa');
+
+caixaElement.style.width = '200px';
+caixaElement.style.height = '200px';
+caixaElement.style.backgroundColor = 'green';
+
+/*
+ As configurações de estilos via JavaScript seguem 
+ os mesmos conceitos e atributos utilizados via CSS.
+
+ OBS: Diferença
+
+ Em CSS onde usa-se background-color
+ Em JavaScript usa-se backgroundColor
+
+ Outro exemplo:
+
+ Em CSS onde usa-se font-size
+ Em JavaScript usa-se fontSize
+
+ Mais um exemplo:
+
+ Em CSS onde usa-se margin-bottom
+ Em JavaScript usa-se marginBottom
+ 
+*/
