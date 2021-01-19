e8e871dd400ef6f500b26a10e918c1e656d13ee3
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-19 11:15:36 -0300

    add Alterando Estilos.

---
+++ manipulando-DOM/elementos-da-pagina.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector('button');
+
+btn.onclick = function(){
+    // Declara a variável de texto que será adicionado ao parágrafo
+    let textElement = '';
+
+    // Recupera o valor do input
+    let inputValue = document.querySelector('input').value;
+
+    // Cria um elemento parágrafo <p></p>
+    let pElement = document.createElement('p');
+
+    // Adiciona o atributo class ao parágrafo
+    pElement.setAttribute('class', 'classe-p');
+
+    // Verifica se o usuário preencheu o input, caso contrário coloca um texto padrão
+    if(inputValue !== ""){
+        textElement = document.createTextNode(inputValue);
+    }else{
+        textElement = document.createTextNode('Veio vazio....');
+    }
+
+    // Adiciona o texto como filho do parágrafo
+    pElement.appendChild(textElement);
+
+    // Recupera a div
+    let divElement = document.querySelector('#app');
+
+    // Adiciona o parágrafo como filho da div
+    divElement.appendChild(pElement);
+
+    // Limpa o input 
+    document.querySelector('input').value = '';
+}
---
+++ manipulando-DOM/elementos-da-pagina2.js
@@ -0,0 +1,9 @@
+function removerBotao(btn){
+    // Recuperar o elemento div
+    //let divElement = document.querySelector('#app');
+
+    // Remove o filho selecionado da div
+    //divElement.removeChild(btn);
+
+    document.querySelector('#app').removeChild(btn); // mesmo codigo acima feito numa linha só.
+}
