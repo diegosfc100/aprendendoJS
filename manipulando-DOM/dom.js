264364091cb9dd2c8f692b4d553d6c8b1dad7dff
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-19 11:11:06 -0300

    add O que é DOM?

---
+++ manipulando-DOM/dom.js
@@ -0,0 +1,25 @@
+// document( é Todo documento html)
+// querySelector faz parte da api javascript.
+let a = document.querySelector('.link');
+
+//console.log(a.innerText);
+
+//console.log(a.parentNode); //body (parentNode = parente do nó = pai)
+
+//console.log(a.parentNode.parentNode); //html
+
+// console.log(a.parentNode.parentNode.parentNode);// Document
+
+//console.log(a.parentNode.parentNode.parentNode.parentNode);// null
+
+//console.log(a.parentNode.parentNode.childNodes); // child = filhos.
+
+//console.log(a.parentNode.parentNode.firstChild); // head
+
+//console.log(a.firstChild); 
+
+//console.log(a.parentNode.parentNode.lastChild); // body
+
+//console.log(a.parentNode.parentNode.firstChild.nextSibling.firstChild.nextSibling);
+
+//console.log(a.parentNode.parentNode.firstChild.nextSibling.previousSibling);
