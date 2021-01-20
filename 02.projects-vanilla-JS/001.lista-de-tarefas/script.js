4087043dbde983dfa8dab206a2955c6ccee0e4dd
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-20 08:19:38 -0300

    iniciando aplicação

---
+++ 02.projects-vanilla-JS/001.lista-de-tarefas/script.js
@@ -0,0 +1,32 @@
+// 1. Referenciando input
+
+let input = document.querySelector('input[name=tarefa]');
+
+// 2. Referenciando button
+
+let btn = document.querySelector('#botao');
+
+// 3. Referenciando a list
+let lista = document.querySelector('#lista');
+
+
+let tarefas = [
+    'Comer',
+    'Dormir',
+    'Rezar',
+    'Estudar',
+    'Trabalhar'
+]
+
+
+
+
+/*
+     <li class="list-group-item list-group-item-action">Dormir</li>
+                    <li class="list-group-item list-group-item-action">Comer</li>
+                    <li class="list-group-item list-group-item-action">Rezar</li>
+                    <li class="list-group-item list-group-item-action">Trabalhar</li>
+                    <li class="list-group-item list-group-item-action">Estudar</li>
+                    <li class="list-group-item list-group-item-action">Transar</li>
+
+                    */
