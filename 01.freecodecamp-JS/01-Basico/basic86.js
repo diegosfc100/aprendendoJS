5781d0c8ccdb3c51ea0c4a296d9162501d196d5e
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-21 11:33:02 -0300

    add exercício 86

--- 01.freecodecamp-JS/01-Basico/basic86.js
+++ 01.freecodecamp-JS/01-Basico/basic86.js
@@ -1,5 +1,39 @@
-/* Excluir propriedades de um objeto JavaScript
+/* Excluir propriedades de um objeto JavaScript.
+
+Também podemos excluir propriedades de objetos como este:
+
+delete ourDog.bark;
+
+Exemplo: */
+
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"],
+    "bark": "bow-wow"
+  };
+
+delete ourDog.bark;
+
+/* Após a última linha mostrada acima, nosso cachorro se parece com:
+
+{
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+}
 
 
+Exclua a propriedade "tails" de myDog. Você pode usar a notação de ponto ou colchete. */
 
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
 
+  delete myDog.tails;
