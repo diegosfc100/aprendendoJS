bef43c744d563f587de3addb842e95dbe538fd4c
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-27 14:14:53 -0300

    add Object short sintax

---
+++ es6+/object-short-syntax.js
@@ -0,0 +1,12 @@
+const nome = 'Programação em JavaScript';
+const preco = 'R$ 27,99';
+const horas = 25;
+
+const curso = {
+    nome,
+    preco,
+    instrutor: 'Geek University',
+    carga_horaria: horas,
+};
+
+console.log(curso);
