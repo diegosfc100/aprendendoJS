7f00bc5601cacc12f0390afd68cef461626a0eac
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-28 12:28:53 -0300

    add exercício 1.3

--- logica-de-programacao/01-variaveis-e-operadores/exercicio1.3.js
+++ logica-de-programacao/01-variaveis-e-operadores/exercicio1.3.js
@@ -1,41 +1,13 @@
-//pedir para o usuario digitar um numero.
+/* Pedir para o usuário digitar dois números e mostrar uma msg informando
+se foi aprovado ou não. A nota de corte é 5. */
 
-//converta o número para float.
-
-//pedir para o usuario digitar outro numero.
-
-//converta o número para float.
-
-//crie uma varável com a média entre os dois números
-
-// mostrar num alert se a média é maior do que n.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// O EXERCÍCIO RESOLVIDO ESTÁ LOGO ABAIXO, RESOLVA PRIMEIRO E DEPOIS COMPARE. //
-
-
-
-var n1 = parseFloat(prompt("Digite um número:"))
-
-var n2 = parseFloat(prompt("Digite outro número:"))
+var n1 = parseFloat(prompt("Digite sua primeira nota: "))
+var n2 = parseFloat(prompt("Digite sua segunda nota: "))
 
 var media = (n1 + n2) / 2
 
-alert(media > 10)
+if(media >= 5){
+    alert("Você foi aprovado com a nota media de " + media)
+}else{
+    alert("Você foi reprovado com a nota média de " + media)
+}
