1258fad383f611f9bf480555e4dccd2818f43443
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-28 12:14:55 -0300

    add exercício 1.3

---
+++ logica-de-programacao/01-variaveis-e-operadores/exercicio1.3.js
@@ -0,0 +1,41 @@
+//pedir para o usuario digitar um numero.
+
+//converta o número para float.
+
+//pedir para o usuario digitar outro numero.
+
+//converta o número para float.
+
+//crie uma varável com a média entre os dois números
+
+// mostrar num alert se a média é maior do que n.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// O EXERCÍCIO RESOLVIDO ESTÁ LOGO ABAIXO, RESOLVA PRIMEIRO E DEPOIS COMPARE. //
+
+
+
+var n1 = parseFloat(prompt("Digite um número:"))
+
+var n2 = parseFloat(prompt("Digite outro número:"))
+
+var media = (n1 + n2) / 2
+
+alert(media > 10)
