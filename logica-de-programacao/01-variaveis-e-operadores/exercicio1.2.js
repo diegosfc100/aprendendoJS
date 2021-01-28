9cd94d78c4facc8824ce455112775e7042041954
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-01-28 11:19:52 -0300

    add exercício 1.2

---
+++ logica-de-programacao/01-variaveis-e-operadores/exercicio1.2.js
@@ -0,0 +1,40 @@
+//pedir para o usuario digitar um numero
+                                                            
+//converter em numero inteiro
+                                                
+//pedir para o usuario digitar outro numero
+                        
+//converter em numero inteiro
+                        
+// mostrar num alert o seguinte texto: o resto da divisão de (n1) por (n2) é: (resultado); 
+            
+// mostrar num alert o seguinte texto o numero (n1) elevado a (n2) é: (resultado); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// O EXERCÍCIO RESOLVIDO ESTÁ LOGO ABAIXO, RESOLVA PRIMEIRO E DEPOIS COMPARE. //
+
+
+var n1 = prompt("Digite um número:");
+n1 - parseInt(n1);
+
+var n2 = prompt("Digite um número:");
+n2 - parseInt(n2);
+
+alert(`o resto da divisão de ${n1} por ${n2} é: ${n1 % n2}`)
+alert(`o número ${n1} elevado a ${n2} é: ${n1 ** n2}`)
