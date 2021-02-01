38b8dd9910342f860a15ac23784ccac3f6eab992
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-01 12:40:10 -0300

    add aula02 - operadores lógicos

---
+++ 03-JS-ninja/aula02.js
@@ -0,0 +1,11 @@
+// OPERADORES LÓGICOS:
+
+// && Operador "and"
+x==3 && y==4    // (x igual a 3 e y igual a 4)
+
+// || Operador "or"
+x==3 || y==4    // (x igual a 3 ou y igual a 4)
+
+// ! Operador "not"
+x !== 3 // false
+x !== '3' // true
