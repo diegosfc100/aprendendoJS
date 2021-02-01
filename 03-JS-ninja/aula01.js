eb8b6f612d6747aedb8f51897f657f86b233a7bf
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-01 09:48:27 -0300

    add aula01-váriaveis e tipos de dados

---
+++ 03-JS-ninja/aula01.js
@@ -0,0 +1,30 @@
+// Alguns tipos de valores
+
+null // Nenhum valor (um valor nulo)
+undefined // Ausencia de valor (nao existe)
+NaN // Not a number, quando deveria ser um numero, mas nao e
+
+
+// Object {}
+// Objetos tem propriedades e metodos.
+    var pessoas = {
+        altura: 1.87, //propriedades sao coisas referentes ao objeto, no caso a pessoa
+        peso: 90,
+    };
+        
+// propriedades podem ser alteradas
+    pessoas.altura = 1.95;
+
+
+// Array []
+// Arrays sao listas, ou vetores, de valores
+    var numeros = [];
+    var numeros = [1,2,3,4,5];
+    var letras = ['a','b','c','d','e']
+    
+// indices do Array
+    numeros[0] // 1 a
+    numeros[1] // 2 b
+    numeros[2] // 3 c
+    numeros[3] // 4 d
+    numeros[4] // 5 e
