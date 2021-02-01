4aa17b706ea0c44d106dde94ad2b11ae8540eaa9
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-01 12:53:58 -0300

    add aula02 - Estrutura Léxica

--- 03-JS-ninja/aula02.js
+++ 03-JS-ninja/aula02.js
@@ -1,11 +1,42 @@
 // OPERADORES LÓGICOS:
 
-// && Operador "and"
-x==3 && y==4    // (x igual a 3 e y igual a 4)
+    // && Operador "and"
+    x==3 && y==4    // (x igual a 3 e y igual a 4)
 
-// || Operador "or"
-x==3 || y==4    // (x igual a 3 ou y igual a 4)
+    // || Operador "or"
+    x==3 || y==4    // (x igual a 3 ou y igual a 4)
 
-// ! Operador "not"
-x !== 3 // false
-x !== '3' // true
+    // ! Operador "not"
+    x !== 3 // false
+    x !== '3' // true
+
+// + OPERADOR UNARIO
+    // usado para converter em número variáveis que são NaN
+    +'3' // 3
+    // usado para concatenação, sempre que tiver um + somando uma string ele irá retornar uma concatenação
+        'cla' + 'ra' // 'clara'
+        '3' +  3 // '33'
+
+// - INVERSOR DE SINAL
+    // também converte a string para número
+        -'3' // -3
+
+
+// ESTRUTURA LÉXICA
+
+    //Case Sensitive, quer dizer que a linguagem diferencia as letras maiúsculas das minúsculas.
+
+
+// Literais
+    // Valores que não mudam, que fazem parte da linguagem
+    12 // 12
+    true
+    'strings'
+    [arrays]
+
+// Identificadores
+// Nomes para variáveis e funções
+    // podem iniciar
+        "_" , "$" , "letras de 'a' a 'z'" , "letras de 'A' a 'Z'"
+    // Podem conter
+        "_" , "$" , "letras de 'a' a 'z'" , "letras de 'A' a 'Z'" , "digitos de 0 a 9"
