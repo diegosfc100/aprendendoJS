f561e61d30e8372b45b1d4f835a3fd73e925e429
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-03 16:53:40 -0300

    add aula04 - truthy e falsy; condicional ternário e escopo de variaveis

---
+++ 03-JS-ninja/aula04.js
@@ -0,0 +1,39 @@
+// FALSY
+    // Valor que quando convertido para booleano vira false  
+    undefined
+    null
+    NaN
+    false
+    0
+    -0
+    ' ' ou " " // Strings vazias
+  
+// TRUTHY
+// Valor que quando convertido para booleano vira true
+    {} //objetos vazios
+    [] //arrays vazios
+    'strings' "não vazias"
+    // todos os outros valores
+
+
+// Testando
+// Usando if
+    var teste;
+    if( /* coisa a ser testada */ ){
+        teste = true;
+    } else {
+        teste = false;
+    }
+    
+// Usando !!
+    !!true // true
+    !!'clara' // true
+    !!0 // false
+    !!' ' // false
+
+
+// Condicional Ternário 
+   // condição ? true;
+   // condição : false;
+   1 === 2 ? true : false; // false
+   var sexo = pessoa.sexo === 'Feminino' ? 'a': 'o';
