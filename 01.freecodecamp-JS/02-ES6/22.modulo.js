f08c9ebce179339ea8394eac625d40bb0b89a821
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-10 16:59:39 -0300

    add exercício

---
+++ 01.freecodecamp-JS/02-ES6/22.modulo.js
@@ -0,0 +1,23 @@
+/* Crie um script de módulo
+
+O JavaScript começou com um pequeno papel a desempenhar em uma web, de outra forma, principalmente HTML.
+Hoje, é enorme e alguns sites são construídos quase inteiramente com JavaScript. Para tornar o JavaScript mais modular,
+limpo e fácil de manter; ES6 introduziu uma maneira de compartilhar códigos facilmente entre arquivos JavaScript.
+Isso envolve a exportação de partes de um arquivo para uso em um ou mais outros arquivos e a importação das partes
+de que você precisa, onde precisar. Para aproveitar essa funcionalidade, você precisa criar um script em seu 
+documento HTML com um tipo de módulo. Aqui está um exemplo: */
+
+<script type="module" src="filename.js"></script>
+
+/* Um script que usa esse tipo de módulo agora pode usar os recursos de importação e exportação que você aprenderá
+nos próximos desafios.
+
+Adicione um script ao documento HTML do módulo de tipo e forneça a ele o arquivo de origem index.js */
+
+/*
+<html>
+  <body>
+    <script type="module" src="index.js"></script>
+  </body>
+</html>
+*/
