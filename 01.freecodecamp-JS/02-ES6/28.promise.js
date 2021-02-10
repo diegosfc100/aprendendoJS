5d1f645a6b63f5290addf145da66409c4d1e6530
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-10 17:01:16 -0300

    add exercício

---
+++ 01.freecodecamp-JS/02-ES6/28.promise.js
@@ -0,0 +1,18 @@
+/* Crie uma promessa de JavaScript
+
+Uma promessa em JavaScript é exatamente o que parece - você a usa para fazer uma promessa de fazer algo,
+geralmente de forma assíncrona. Quando a tarefa é concluída, você cumpre sua promessa ou deixa de fazê-lo. 
+Promise é uma função construtora, então você precisa usar a nova palavra-chave para criar uma. Ele recebe uma função,
+como argumento, com dois parâmetros - resolve e reject. Esses são métodos usados para determinar o resultado da promessa.
+A sintaxe é semelhante a esta: */
+
+const myPromise = new Promise((resolve, reject) => {
+
+});
+
+
+/* Crie uma nova promessa chamada makeServerRequest. Passe uma função com parâmetros de resolução e rejeição para o construtor.*/
+
+const makeServerRequest = new Promise((resolve, reject)=> {
+
+})
