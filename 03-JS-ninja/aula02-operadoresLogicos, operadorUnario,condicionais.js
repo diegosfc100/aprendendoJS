b5787dd3965f3ff4801324157ff341367db67028
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-23 11:07:00 -0300

    revisão da aula 02

--- 03-JS-ninja/aula02-operadoresLogicos, operadorUnario,condicionais.js
+++ 03-JS-ninja/aula02-operadoresLogicos, operadorUnario,condicionais.js
@@ -1,61 +1,68 @@
 // OPERADORES LÓGICOS:
+    var x = 3, y = 4;
 
     // && Operador "and"
-    x==3 && y==4    // (x igual a 3 e y igual a 4)
+    x == 3 && y == 4
 
     // || Operador "or"
-    x==3 || y==4    // (x igual a 3 ou y igual a 4)
+    x == 3 || y == 4 
 
     // ! Operador "not"
     x !== 3 // false
     x !== '3' // true
-
-// + OPERADOR UNARIO
+    
+    
+    
+// OPERADOR UNARIO
     // usado para converter em número variáveis que são NaN
     +'3' // 3
+    +'Diego' // NaN
+
     // usado para concatenação, sempre que tiver um + somando uma string ele irá retornar uma concatenação
         'cla' + 'ra' // 'clara'
-        '3' +  3 // '33'
+        '3' + 3 // '33'
 
-// - INVERSOR DE SINAL
-    // também converte a string para número
-        -'3' // -3
-
-
+    // Inversor de sinal
+    var a = '5'
+    -a // -5
+    
+    
+    
 // ESTRUTURA LÉXICA
 
-    //Case Sensitive, quer dizer que a linguagem diferencia as letras maiúsculas das minúsculas.
-
-
-// Literais
-    // Valores que não mudam, que fazem parte da linguagem
-    12 // 12
-    true
-    'strings'
-    [arrays]
-
-// Identificadores
-// Nomes para variáveis e funções
-    // podem iniciar
+    // Identificadores
+    // Nomes para variáveis e funções
+    // podem iniciar com ..
         "_" , "$" , "letras de 'a' a 'z'" , "letras de 'A' a 'Z'"
-    // Podem conter
+    // podem conter ..
         "_" , "$" , "letras de 'a' a 'z'" , "letras de 'A' a 'Z'" , "digitos de 0 a 9"
-
-
-
+        
+        
+        
 // CONDICIONAIS
-    // If
+    // IF
     /* if(se isso for verdadeiro){
-        Faça isso
+        faça isso
     } */
     
-// Else
+    // ELSE
     /*else{
         Se o if não acontecer, faça isso..
     } */
 
-// Else If
+    // ELSE IF
     /*else if (próxima condição) {
         É a próxima condição se o if for falso antes de acontecer o else. Pode ser usado quantas vezes quiser. 
     } */
 
+    function teste(idade){
+        if(idade >= 18){
+            return 'Maior de Idade'
+        }else if(idade >= 15 && idade < 18){
+            return "Adolescente e Menor de Idade"
+        }else{
+            return 'Criança'
+        }
+    }
+
+    console.log(teste(18));
