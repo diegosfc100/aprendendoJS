cdec331947e2b0ff0cdeca2ccc96af2f1127309d
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-25 09:47:58 -0300

    add resumo aula02

---
+++ 04-curso-JS-roger-melo/Etapa02-Controle-de-fluxo/aula02.js
@@ -0,0 +1,47 @@
+// Estrutura condicional - IF, ELSE, ELSE IF
+
+    let age = 18;
+
+    if (age >= 18) {
+        console.log('Você tem 18 ou mais');
+     }
+    else if (age > 16) {
+        console.log('Você tem mais de 16 anos');
+    }
+    else {
+        console.log('Você tem 16 anos ou menos');
+    }
+
+    // const temVulnerabilidadeEconomica = false;
+    // const eEstudante = true;
+
+    // // Operadores Lógicos 
+
+    // // Operador && (e)
+
+    // const eCrianca = false;
+    // const eIdoso = true;
+    // const temDinheiro = false;
+
+    // if (temDinheiro && (eCrianca || eIdoso)) {
+        
+    //   // Não foi exercutado
+    //   console.log("Meia entrada liberada");
+    //   // Agora tem que ter dinheiro e ser uma criança ou idoso.
+    //   // Como tem &&, precisamos que a sentença da esquerda e direita dê true
+    // }
+
+    // Operadores Lógicos 
+
+    // Operador && (e)
+
+    const maiorDeIdade = true;
+    const temDinheiro = true;
+
+    if (temDinheiro && maiorDeIdade) {
+
+    // Foi executado
+    console.log("Meia entrada liberada");
+    // Agora tem que ter dinheiro e ser maior de idade
+    // Como tem &&, precisamos que a sentença da esquerda e direita dê true
+    }
