7c66f3491c8b7a6905ee1ccf27bbd5b830649a7d
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-25 09:48:59 -0300

    add resumo aula03

--- 04-curso-JS-roger-melo/Etapa04-Objetos/aula03.js
+++ 04-curso-JS-roger-melo/Etapa04-Objetos/aula03.js
@@ -1,55 +1,48 @@
-//objeto MATH - é um objeto que contém métodos e propriedades matemáticas/
-//alguns MÉTODOS/
-const area = 7.7
-console.log(Math.round(area)) //arredonda um número. Se menor que 5 arredonda pra baixo, se maior ou igual 5 arredonda pra cima
-console.log(Math.floor(area)) //sempre arredonda pra baixo independente da parte decimal.
-console.log(Math.ceil(area)) //sempre arredonda pra cima independente da parte decimal.
-console.log(Math.trunc(area)) //vai remover a parte decimal e retorna a parte inteira.
+//OBJETOS EM ARRRAYS
 
-/*números aleatórios*/
-    const randomNumber = Math.random()
-    console.log(randomNumber)
-    console.log(Math.round(randomNumber * 100)) //obtendo número aleatório entre 0 e 100
+    const filmes = {
+        acao: [
+             { title: 'Rambo', visualizacoes: 25 },
+             { title: '007', visualizacoes: 55 }
+        ],
+        logFilmesAcao() {
+            console.log('Esses são os filmes de ação mais assistidos: ');
+            
+        this.acao.forEach(filme => 
+            console.log(`${filme.title} tem ${filme.visualizacoes} visualizações.`))
+        }
+    }
 
-/* TIPOS DE REFERÊNCIA E TIPOS PRIMITIVOS*/
-//stack e heap fazem parte da memória./
+    filmes.logFilmesAcao();
 
-/*PRIMITIVOS                               
-    1 - Numbers
-    2 - Strings
-    3 - Booleans
-    4 - Null
-    5 - Undefined
-    6 - Symbol
-    7 - BigInt
 
-    * Armazenados na STACK - São mais rápidos, porém menores.
-*/
-    let scoreOne = 10
-    let scoreTwo = scoreOne
-    console.log(`ScoreOne: ${scoreOne} | ScoreTwo: ${scoreTwo}`)
+
+// TIPOS PRIMITIVOS
+
+    let pontoUm = 50;
+    let pontoDois = pontoUm;
+    /* Apesar de "pontoDois" estar atribuindo o valor da variável "pontoUm" para ele, nesse momento está
+       sendo feito uma copia do valor, mas com um novo lugar na memória. */
+       
+    console.log(`pontoUm: ${pontoUm} | pontoDois: ${pontoDois}`);
+
+
+    pontoUm = 100;  // Vai ser alterado somente o valor de "pontoUm"
+    console.log(`pontoUm: ${pontoUm} | pontoDois: ${pontoDois}`);
     
-    scoreOne = 222
-    console.log(`ScoreOne: ${scoreOne} | ScoreTwo: ${scoreTwo}`)
-    // como são valores primitivos (é feito cópia), scoreOne não afeta scoreTwo e vice-versa.
 
-/*REFERÊNCIA (Todos os tipos de objetos)
-    - Objetos Literais
-    - Arrays
-    - Funções
-    - Datas
-    - Todos outros objetos
 
-    * Armazenados na HEAP - São mais lentos, porém maiores.
-*/
-    let userOne = {name: 'gabriel', age: 30}
-    let userTwo = userOne
+    // Tipos de referência
 
-    console.log(userOne, userTwo)
-    
-    userTwo.name = 'Joãozinho'
-    userTwo.age = 12
+    /* Apesar de "pontoDois" aparentemente receber um valor, na verdade, ele está recebendo o mesmo ponteiro para
+       o objeto, ou seja, é o mesmo objeto na memória. Sendo assim, se eu alterar uma das propriedades do objeto 
+       em uma das variáveis, eu estaria alterando para as duas variáveis. */
 
-    console.log(userOne, userTwo)
-    // como são valores por referência (é passado referência da memória), ao atribuir um objeto ao outro eles compartilham
-    // seu endereço, é por isso que ao trocar qualquer propriedade de um afeta o outro.
+    pontoUm = { nome: "Bernardo", idade: 19 };
+    pontoDois = pontoUm;
+
+    console.log(pontoUm, pontoDois);
+
+    pontoUm.idade = 20;
+
+    console.log(pontoUm, pontoDois);
