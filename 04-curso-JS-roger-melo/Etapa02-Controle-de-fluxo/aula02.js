6c9094b4de2117021aa7e5905b383df31edd8455
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-01 10:29:10 -0300

    update aula 02

--- 04-curso-JS-roger-melo/Etapa02-Controle-de-fluxo/aula02.js
+++ 04-curso-JS-roger-melo/Etapa02-Controle-de-fluxo/aula02.js
@@ -45,3 +45,30 @@
     // Agora tem que ter dinheiro e ser maior de idade
     // Como tem &&, precisamos que a sentença da esquerda e direita dê true
     }
+
+
+
+//SWITCH
+
+
+    const grade = 'V'
+
+    switch (grade) {
+    case 'A':
+        console.log('Parabéns, você tirou um A!')
+        break
+    case 'B':
+        console.log('Parabéns, você tirou um B!')
+        break
+    case 'C':
+        console.log('Parabéns, você tirou um C!')
+        break
+    case 'D':
+        console.log('Parabéns, você tirou um D!')
+        break
+    case 'E':
+        console.log('Parabéns, você tirou um E!')
+        break
+    default:
+        console.log('Nota inválida')
+    }
