8bbc37b845dbd6d38261231571f639986a7ed377
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-01 08:11:00 -0300

    add exercício 30

---
+++ 01.freecodecamp-JS/03-Regex/30.agrupamentoMistoDeCaracteres.js
@@ -0,0 +1,21 @@
+/* Verifique se há agrupamento misto de caracteres
+
+Às vezes, queremos verificar grupos de caracteres usando uma Expressão Regular e, para isso, usamos parênteses ().
+Se quiser encontrar Penguin ou Pumpkin em uma string, você pode usar a seguinte Expressão Regular: /P(engu|umpk)in/g
+
+Em seguida, verifique se os grupos de strings desejados estão na string de teste usando o método test().
+
+let testStr = "Pumpkin";
+let testRegex = /P(engu|umpk)in/;
+testRegex.test(testStr);
+// Returns true
+
+
+/* Corrija o regex para que verifique os nomes de Franklin Roosevelt ou Eleanor Roosevelt de maneira sensível
+a maiúsculas e minúsculas e faça concessões para nomes do meio.
+
+Em seguida, corrija o código para que a regex que você criou seja verificada em relação a myString
+e true ou false seja retornado, dependendo da correspondência da regex. */
+
+
+
