a469b7a398758f00e5ab3255d14e6968aafb78e7
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-01 09:32:02 -0300

    add exercício 33

---
+++ 01.freecodecamp-JS/03-Regex/33.removeWhitespace.js
@@ -0,0 +1,11 @@
+/* Remova os espaços em branco do início e do fim
+
+Às vezes, caracteres de espaço em branco em torno de strings não são desejados, mas existem.
+O processamento típico de strings consiste em remover os espaços em branco no início e no final dele.
+
+Escreva uma regex e use os métodos de string apropriados para remover os espaços em branco no início e no final 
+das strings.
+
+Nota: O método String.prototype.trim() funcionaria aqui, mas você precisará completar este desafio usando expressões 
+regulares. */
+
