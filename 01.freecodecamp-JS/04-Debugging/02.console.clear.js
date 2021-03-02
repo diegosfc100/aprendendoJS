b3b62b409ee702527bd5f1cceda2f454f9396fc1
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-02 09:42:05 -0300

    add exercício 02

---
+++ 01.freecodecamp-JS/04-Debugging/02.console.clear.js
@@ -0,0 +1,25 @@
+/* Compreendendo as diferenças entre o freeCodeCamp e o console do navegador
+
+Você deve ter notado que alguns desafios de JavaScript do freeCodeCamp incluem seu próprio console.
+Este console se comporta de maneira um pouco diferente do console do navegador que você usou no último desafio.
+
+O desafio a seguir pretende destacar a principal diferença entre o console do freeCodeCamp e o console do seu navegador.
+Quando você executa o JavaScript comum, o console do navegador exibe suas instruções console.log() o número exato de
+vezes que é chamado.
+
+O console freeCodeCamp imprimirá suas instruções console.log() logo após o editor detectar uma alteração no script,
+bem como durante o teste.
+
+O console freeCodeCamp é limpo antes de os testes serem executados e, para evitar spam, apenas imprime os logs durante 
+o primeiro teste (veja a nota abaixo para exceções).
+
+Se quiser ver todos os registros de todos os testes, execute os testes e abra o console do navegador. Se você preferir
+usar o console do navegador e quiser que ele imite o console do freeCodeCamp, coloque console.clear() antes de qualquer
+outra chamada de console para limpar o console do navegador.
+
+Nota: console.logs dentro das funções são impressos no console freeCodeCamp sempre que essas funções são chamadas,
+isso pode ajudar a depurar funções que são chamadas durante o teste.
+
+Primeiro, use console.log para registrar a variável de saída. Em seguida, use console.clear para limpar o console do 
+navegador. */
+
