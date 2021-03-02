90f7c80a95d1cef3b119ea8b4869f130460bd4f3
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-02 09:52:30 -0300

    add exercício 03

---
+++ 01.freecodecamp-JS/04-Debugging/03.typeof.js
@@ -0,0 +1,21 @@
+/* Use typeof para verificar o tipo de uma variável
+
+Você pode usar typeof para verificar a estrutura de dados, ou tipo, de uma variável. Isso é útil na depuração ao 
+trabalhar com vários tipos de dados. Se você pensa que está adicionando dois números, mas um deles é na verdade uma
+string, os resultados podem ser inesperados. Erros de tipo podem se esconder em cálculos ou chamadas de função.
+Tenha cuidado especialmente ao acessar e trabalhar com dados externos na forma de um objeto JavaScript Object Notation 
+(JSON).
+
+Aqui estão alguns exemplos usando typeof:
+
+console.log (typeof ""); // produz "string"
+console.log (typeof 0); // produz "número"
+console.log (typeof []); // produz "objeto"
+console.log (typeof {}); // produz "objeto"
+
+JavaScript reconhece seis tipos de dados primitivos (imutáveis):
+Boolean, Null, Undefined, Number, String e Symbol(novo com ES6) e um tipo para itens mutáveis: Object. 
+Observe que, em JavaScript, os arrays são tecnicamente um tipo de objeto.
+
+Adicione duas instruções console.log() para verificar o tipo de cada uma das duas variáveis ​​sete e três no código. */
+
