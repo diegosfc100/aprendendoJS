343e2c66071b6e0a1d7af23a8b426c66edd96802
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-02 09:54:51 -0300

    add exercício 04

---
+++ 01.freecodecamp-JS/04-Debugging/04.errosOrtograficos.js
@@ -0,0 +1,11 @@
+/* Capturar nomes de variáveis e funções com erros ortográficos
+
+Os métodos console.log() e typeof são as duas formas principais de verificar os valores intermediários e os tipos de 
+saída do programa. Agora é hora de entrar nas formas comuns que os bugs assumem. Um problema de nível de sintaxe com
+o qual digitadores rápidos podem se lamentar é o humilde erro de ortografia.
+
+Caracteres transpostos, ausentes ou mal capitalizados em uma variável ou nome de função farão com que o navegador
+procure um objeto que não existe - e reclamará na forma de um erro de referência. Os nomes de variáveis e funções 
+JavaScript diferenciam maiúsculas de minúsculas.
+
+Corrija os dois erros de ortografia no código para que o cálculo netWorkingCapital funcione. */
