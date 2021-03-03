da96f463f7dc70aa320a6eaff7cae391d7108e24
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-03 07:11:26 -0300

    add solução exercício 03

--- 01.freecodecamp-JS/05-DataStructures/03.push-unshift.js
+++ 01.freecodecamp-JS/05-DataStructures/03.push-unshift.js
@@ -34,4 +34,11 @@ console.log(mixedNumbers(['IV', 5, 'six']));
 
 */
 
+function mixedNumbers(arr) {
+  arr.unshift('I', 2, 'three');
+  arr.push(7, 'VIII', 9);
 
+return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
