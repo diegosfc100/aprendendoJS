1f9db02101ff2c74c6b7f549f79ff47c6f112108
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 11:05:46 -0300

    add solução exercício 19

--- 01.freecodecamp-JS/05-DataStructures/19.object.keys.js
+++ 01.freecodecamp-JS/05-DataStructures/19.object.keys.js
@@ -36,3 +36,28 @@ console.log(getArrayOfUsers(users));
 
 */
 
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+    return Object.keys(obj)
+
+}
+
+console.log(getArrayOfUsers(users));
