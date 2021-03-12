a2f3697206cb8f22a7974041caab271cdbbbdcd8
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 10:50:58 -0300

    add solução exercício 17

--- 01.freecodecamp-JS/05-DataStructures/17.hasOwnProperty,In.js
+++ 01.freecodecamp-JS/05-DataStructures/17.hasOwnProperty,In.js
@@ -42,3 +42,37 @@ function isEveryoneHere(obj) {
 console.log(isEveryoneHere(users));
 
 */
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(obj) {
+  if('Alan' in users && 'Jeff' in users && 'Sarah' in users && 'Ryan' in users ){
+      return true 
+  } return false;
+}
+
+console.log(isEveryoneHere(users));
+
+/*
+  return users.hasOwnProperty('Alan') &&
+  users.hasOwnProperty('Jeff') &&
+  users.hasOwnProperty('Sarah') &&
+  users.hasOwnProperty('Ryan');
+*/
