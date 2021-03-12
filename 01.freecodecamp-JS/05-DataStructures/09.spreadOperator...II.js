3110e2accf9b60f915890c0171499d376f0c0bab
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 08:43:56 -0300

    add exercício 09

---
+++ 01.freecodecamp-JS/05-DataStructures/09.spreadOperator...II.js
@@ -0,0 +1,29 @@
+/* Combine Arrays com o Operador Spread
+
+Outra grande vantagem do operador spread é a capacidade de combinar arrays, ou inserir todos os elementos de um
+array em outro, em qualquer índice. Com sintaxes mais tradicionais, podemos concatenar matrizes, mas isso apenas
+nos permite combinar matrizes no final de uma e no início de outra. A sintaxe de propagação torna a seguinte operação
+extremamente simples:
+
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+
+thatArray teria o valor ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander'].
+
+Usando a sintaxe de propagação, acabamos de alcançar uma operação que teria sido mais complexa e detalhada
+se tivéssemos usado métodos tradicionais.
+
+Definimos uma função spreadOut que retorna a variável 'sentence'. Modifique a função usando o operador de propagação
+ para que ela retorne o array ['learning', 'to', 'code', 'is', 'fun'] :
+
+ function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence; // Change this line
+  return sentence;
+}
+
+console.log(spreadOut());
+
+*/
+
