d188ae6314a7f48b3011d0198cfc706be15a88c2
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 10:01:41 -0300

    add solução exercício 13

--- 01.freecodecamp-JS/05-DataStructures/13.objetos.js
+++ 01.freecodecamp-JS/05-DataStructures/13.objetos.js
@@ -57,3 +57,15 @@ let foods = {
 console.log(foods);
 
 */
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+
+console.log(foods);
