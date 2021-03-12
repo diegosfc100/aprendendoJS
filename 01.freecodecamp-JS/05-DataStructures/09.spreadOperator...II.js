4c127239ac39780e2b19f8bcf04f3e1d17ac7464
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 08:44:28 -0300

    add solução exercício 09

--- 01.freecodecamp-JS/05-DataStructures/09.spreadOperator...II.js
+++ 01.freecodecamp-JS/05-DataStructures/09.spreadOperator...II.js
@@ -27,3 +27,10 @@ console.log(spreadOut());
 
 */
 
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning',...fragment, 'is', 'fun']
+  return sentence;
+}
+
+console.log(spreadOut());
