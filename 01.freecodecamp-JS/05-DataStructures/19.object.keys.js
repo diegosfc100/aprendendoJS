f8b36f09d9a68af5ce3e7e35bbb579b5fa9631be
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 11:03:00 -0300

    add exercício 19

---
+++ 01.freecodecamp-JS/05-DataStructures/19.object.keys.js
@@ -0,0 +1,38 @@
+/* Gere uma Array de todas as chaves de objeto com Object.keys()
+
+Também podemos gerar um array que contém todas as chaves armazenadas em um objeto usando o método Object.keys()
+e passando um objeto como argumento. Isso retornará uma matriz com strings que representam cada propriedade no objeto.
+Novamente, não haverá uma ordem específica para as entradas na matriz.
+
+Termine de escrever a função getArrayOfUsers para que ela retorne um array contendo todas as propriedades do objeto 
+que recebe como argumento.
+
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+
+  // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+*/
+
