a46e840081c14a098d9a52be243833c0d435dc13
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-12 10:21:52 -0300

    add solução exercício 14

--- 01.freecodecamp-JS/05-DataStructures/14.modificandoObjetosAninhados.js
+++ 01.freecodecamp-JS/05-DataStructures/14.modificandoObjetosAninhados.js
@@ -45,3 +45,16 @@ let userActivity = {
 console.log(userActivity);
 
 */
+
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+userActivity.data.online = 45
+
+console.log(userActivity);
