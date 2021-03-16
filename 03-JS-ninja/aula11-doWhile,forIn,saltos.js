f95daa1df04de5e9778ea6e0724a50f5a3723124
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-16 09:15:38 -0300

    update

--- 03-JS-ninja/aula11-doWhile,forIn,saltos.js
+++ 03-JS-ninja/aula11-doWhile,forIn,saltos.js
@@ -1,12 +1,14 @@
-// Do / While
-	// Primeiro entra na instrunção para depois verificar se é verdadeira ou não
+// DO / WHILE
+	// Primeiro entra na instrução para depois verificar se é verdadeira ou não
 	var counter = 1;
 	do{
 		console.log( counter++ ); // ele sempre fará essa primeira instrução
 	}
 	while( counter < 10 ); // para depois verificar a condição
 
-// For / in
+
+
+// FOR / IN
 	//Serve para percorrer propriedades de um objeto. É mais lento que o for normal
 	var car = {
 		brand: 'VW',
@@ -19,7 +21,9 @@
 	}
 	console.log( 'brand in car?', 'brand' in car ); // brand in car? true
 
-// Saltos
+
+	
+// SALTOS
 	// Usados para pular uma parte do código
 	return // quando implementado pula todo o código seguinte da função
 	break // sai do loop, como por exemplo em um switch
