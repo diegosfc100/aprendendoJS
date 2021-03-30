cb0d5988761dc7ed2223f49ea0d5c8567058d4d1
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-30 12:05:47 -0300

    add resumo aula 16

--- 03-JS-ninja/aula16.js
+++ 03-JS-ninja/aula16.js
@@ -61,3 +61,82 @@
 			console.log( delete myVar ); // syntax error
 			// Sem o modo estrito ele apenas retornaria false, mas com o modo estrito ele retorna syntax error.
 		})();
+
+	// propriedades e argumentos
+		// No use strict as propriedades de objetos devem ter nomes diferentes.
+		(function(){
+			'use strict'
+			var obj = {
+				prop1: 'prop1',
+				prop1: 'prop2',
+				prop3: 'prop3'
+			};
+			console.log( obj ); // syntax error
+			// Sem o modo estrito a segunda propriedade apenas iria sobrescrever a outra.
+		})();
+		// assim como propriedades os argumentos de funções também devem ter nomes diferentes.
+		(function(){
+			'use strict'
+			function myFunction( a, a, b ){
+				return a + b;
+			}
+			console.log( myFunction( 1, 2, 3 ) ); // syntax error
+			// Sem o modo estrito o primeiro argumento seria sobrescrito, retornando 5.
+		})();
+
+// Objeto String - priedade .length e métodos
+	// .length
+		// Conta a quantidade de caracteres de uma string
+		'clara'.length // 5
+
+	// charAt(index)
+		// Retorna a letra passada como parametro
+		'clara'.charAt(0); // 'c'
+
+	// array syntax
+		'clara'[1] // 'l'
+
+	// concat()
+		// concatena strings
+		'clara'.concat( ' verena', ' meu', ' nome', ' é.' ) // 'clara verena meu nome é'
+		// não midifica a string principal
+		var nome = 'clara';
+		nome.concat( ' str1', ' str2', ' str3' ); // 'clara str1 str2 str3'
+		console.log( nome ) // 'clara'
+
+	// indexOf(string, start)
+		// pesquisa dentro da string retornando a posição, você pode dizer por onde quer começar a pesquisar. -1 quer dizer que não existe
+		nome.indexOf('r'); // 3
+		nome.indexOf('ara'); // 2
+		nome.indexOf('z'); // -1
+		// lastIndexOf(string, start) - mesma coisa apenas busca de trás pra frente
+
+	// replace( suibstituir, substituta )
+		// Substitui um trecho específico da string por outra string sem modificar a string principal
+		nome.replace('r', 'b') // 'claba'
+		console.log( nome ) // 'clara'
+
+	// slice(começo, fim)
+		// pega um trecho da string sem modificar a string principal, você determina aonde quer começar e onde termina, sendo que não inclui a última letra
+		nome.slice(1) // 'lara'
+		nome.slice(1, 3) // 'la'
+
+	// split( separador, limite )
+		// quebra a string e a transforma em um array excluindo o item separador sem midificar a string principal
+		nome = fernando;
+		nome.split() // ['fernando']
+		nome.split('n') // ['fer', 'a', 'do']
+		// você pode usar o split pra separar e o join pra juntar
+		var arr = nome.split('n'); // ['fer', 'a', 'do']
+		arr.join('n') // 'fernando'
+		// Isto server pra fazer subistituições
+		nome.split('n').join('z'); // 'ferzazdo'
+
+	// substring()
+		// faz a mesma coisa que o slice, com a diferença de que se você inverter os valores ele pega do final pro inicio também.
+
+	// toLowerCase()
+		// deixa tudo em caixa baixa, não modifica a string principal
+
+	// toUperCase()
+		// deixa tudo em caixa alta, não modifica a string principal
