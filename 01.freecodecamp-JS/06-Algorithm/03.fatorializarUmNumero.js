07341dbc4c585c46c5230f8435d278b2ff221bbf
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-04-14 11:09:13 -0300

    add exercício 03

---
+++ 01.freecodecamp-JS/06-Algorithm/03.fatorializarUmNumero.js
@@ -0,0 +1,19 @@
+/* Fatorializar um número
+
+Retorna o fatorial do inteiro fornecido.
+
+Se o inteiro for representado pela letra n, um fatorial é o produto de todos os inteiros positivos menores ou iguais] a n.
+
+Os fatoriais são frequentemente representados com a notação abreviada n!
+
+Por exemplo: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Somente inteiros maiores ou iguais a zero serão fornecidos para a função.  
+
+function factorialize(num) {
+  return num;
+}
+
+factorialize(5);
+*/
+
