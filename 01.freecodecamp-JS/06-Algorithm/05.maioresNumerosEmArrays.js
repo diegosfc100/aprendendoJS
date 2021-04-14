b4b9e2135c296606ddec9c2d37b4be5d1f2920fb
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-04-14 11:21:39 -0300

    add solução exercício 05

--- 01.freecodecamp-JS/06-Algorithm/05.maioresNumerosEmArrays.js
+++ 01.freecodecamp-JS/06-Algorithm/05.maioresNumerosEmArrays.js
@@ -13,3 +13,24 @@ function largestOfFour(arr) {
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 */
+
+function largestOfFour(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let max = arr[i][0];
+        for (let j = 0; j < arr[i].length; j++) {
+            if (max < arr[i][j]) {
+                max = arr[i][j];
+            }
+        }
+        newArr.push(max);
+    }
+    return newArr;
+}
+
+largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+]);
