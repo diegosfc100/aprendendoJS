d0ace7fae41730556fa73cbba3262025755bcd22
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-04-14 11:19:44 -0300

    add exercício 05

---
+++ 01.freecodecamp-JS/06-Algorithm/05.maioresNumerosEmArrays.js
@@ -0,0 +1,15 @@
+/* Retorne os maiores números em matrizes
+
+Retorne uma matriz que consiste no maior número de cada submatriz fornecida. 
+Para simplificar, a matriz fornecida conterá exatamente 4 submatrizes.
+
+Lembre-se de que você pode iterar por meio de uma matriz com um loop for simples e acessar cada membro com a sintaxe
+de matriz arr [i]. 
+
+function largestOfFour(arr) {
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+*/
