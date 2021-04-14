00c676fa58f295594e61166f0d9ed6e77219a5cd
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-04-14 11:13:44 -0300

    add solução exercício 03

--- 01.freecodecamp-JS/06-Algorithm/03.fatorializarUmNumero.js
+++ 01.freecodecamp-JS/06-Algorithm/03.fatorializarUmNumero.js
@@ -17,3 +17,12 @@ function factorialize(num) {
 factorialize(5);
 */
 
+function factorialize(num) {
+  if (num == 0) {
+      return 1;
+  }
+
+  return factorialize(num - 1) * num;
+}
+
+console.log(factorialize(5));
