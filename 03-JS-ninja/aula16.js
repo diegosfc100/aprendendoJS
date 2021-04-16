a759133b569e385e5881c299bc81f75790f7c8db
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-04-16 12:05:11 -0300

    add resumo aula 16

--- 03-JS-ninja/aula16.js
+++ 03-JS-ninja/aula16.js
@@ -1,19 +1,23 @@
-// Use strict
+//USE STRICT
 	// diretiva, deve utiliza-la no escopo que você quer que seja restrito, pois ele não irá permitir a criação de variáveis sem o 'var'
 	(function(){
 		'use strict';
-		var myName = 'Clara';
-		console.log(myName); // Clara
+		var myName = 'Diego';
+		console.log(myName); // Diego
 	})();
 	console.log(myName); // erro pois a variável myName está em escopo local
+
+
 	// Sem o 'var' ele aponta o erro para dentro da função, para que você adicione o 'var'.
 	(function(){
 		'use strict';
-		myName = 'Clara';
+		myName = 'Diego';
 		console.log(myName);
 	})();
 	console.log(myName);
-	// with
+
+
+	// WITH
 		// diminui o tamanho do objeto, mas existe um problema, pois as variáveis dentro do with podem se confundir, por isso ele é proibido no 'use strict'
 		(function(){
 			var obj = {
@@ -28,12 +32,14 @@
 				}
 			};
 			console.log(obj.prop1.prop2.prop3);
+
 			with( obj.prop1.prop2.prop3 ){
 				console.log( prop31 );
 			}
 		})();
 
-	// Global this
+
+	// GLOBAL THIS
 		(function(){
 			'use strict'
 			function Person( name, lastName, age ){
@@ -41,10 +47,11 @@
 		        this.lastName = lastName;
 		        this.age = age;
 		    }
-			console.log( new Person( 'Clara', 'Verena', 20 ) ); // este é o jeito correto de chamar, instanciando
-			console.log( Person( 'Clara', 'Verena', 20 ) ); // não instanciando, utilizando como função, você irá receber undefined, o this global retorna nesse valor. O 'use strict' não irá permitir isso.
+			console.log( new Person( 'Diego', 'Rodrigues', 30 ) ); // este é o jeito correto de chamar, instanciando
+			console.log( Person( 'Diego', 'Rodrigues', 30 ) ); // não instanciando, utilizando como função, você irá receber undefined, o this global retorna nesse valor. O 'use strict' não irá permitir isso.
 		})();
 		console.log( this ) // retorna undefined quando não é usado o construtor
+
 
 	// Delete
 		// O delete nem sempre pode ser usado. Ele só deleta propriedades de objetos.
@@ -62,6 +69,7 @@
 			// Sem o modo estrito ele apenas retornaria false, mas com o modo estrito ele retorna syntax error.
 		})();
 
+
 	// propriedades e argumentos
 		// No use strict as propriedades de objetos devem ter nomes diferentes.
 		(function(){
@@ -74,6 +82,8 @@
 			console.log( obj ); // syntax error
 			// Sem o modo estrito a segunda propriedade apenas iria sobrescrever a outra.
 		})();
+
+
 		// assim como propriedades os argumentos de funções também devem ter nomes diferentes.
 		(function(){
 			'use strict'
@@ -84,42 +94,45 @@
 			// Sem o modo estrito o primeiro argumento seria sobrescrito, retornando 5.
 		})();
 
-// Objeto String - priedade .length e métodos
+
+
+// OBJETO STRING
 	// .length
 		// Conta a quantidade de caracteres de uma string
-		'clara'.length // 5
+		'DIEGO'.length // 5
 
 	// charAt(index)
 		// Retorna a letra passada como parametro
-		'clara'.charAt(0); // 'c'
+		'DIEGO'.charAt(0); // 'D'
 
 	// array syntax
-		'clara'[1] // 'l'
+		'DIEGO'[1] // 'I'
 
 	// concat()
 		// concatena strings
-		'clara'.concat( ' verena', ' meu', ' nome', ' é.' ) // 'clara verena meu nome é'
-		// não midifica a string principal
-		var nome = 'clara';
-		nome.concat( ' str1', ' str2', ' str3' ); // 'clara str1 str2 str3'
-		console.log( nome ) // 'clara'
+		'diego'.concat( ' rodrigues', ' meu', ' nome', ' é.' ) // 'diego rodrigues meu nome é'
+		// não modifica a string principal
+
+		var nome = 'diego';
+		nome.concat( ' str1', ' str2', ' str3' ); // 'diego str1 str2 str3'
+		console.log( nome ) // 'diego'
 
 	// indexOf(string, start)
 		// pesquisa dentro da string retornando a posição, você pode dizer por onde quer começar a pesquisar. -1 quer dizer que não existe
-		nome.indexOf('r'); // 3
-		nome.indexOf('ara'); // 2
+		nome.indexOf('d'); // 0
+		nome.indexOf('ego'); // 2
 		nome.indexOf('z'); // -1
 		// lastIndexOf(string, start) - mesma coisa apenas busca de trás pra frente
 
 	// replace( suibstituir, substituta )
 		// Substitui um trecho específico da string por outra string sem modificar a string principal
-		nome.replace('r', 'b') // 'claba'
-		console.log( nome ) // 'clara'
+		nome.replace('d', 't') // 'tiego'
+		console.log( nome ) // 'diego'
 
 	// slice(começo, fim)
 		// pega um trecho da string sem modificar a string principal, você determina aonde quer começar e onde termina, sendo que não inclui a última letra
-		nome.slice(1) // 'lara'
-		nome.slice(1, 3) // 'la'
+		nome.slice(1) // 'iego'
+		nome.slice(1, 3) // 'go'
 
 	// split( separador, limite )
 		// quebra a string e a transforma em um array excluindo o item separador sem midificar a string principal
