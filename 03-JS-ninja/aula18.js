0c72484b2692f41d8d9a7f63e9119c19c3d44eb0
diegosfc100 <diego.sfc100@hotmail.com>
2021-04-30 10:41:26 -0300

    add resumo aula 18

--- 03-JS-ninja/aula18.js
+++ 03-JS-ninja/aula18.js
@@ -1,28 +1,36 @@
-// Regex - Expressões regulares continuação...
+// REGEX - continuação...
+
 	// \s verifica os espaços em branco
 	// \n verifica as quebras de linha
 	// \t verifica as tabulações(tab)
 	// . verifica qualquer caractere exceto quebra de linha
 
+
 	// Negação
 		// ^ nega a sequencia, por exemplo diz que a verificação pode ser feita com todos os itens menos o que está na lista
-        text.match(/[^abc]/);
+        	text.match(/[^abc]/);
 		// a negação pode ser feita também com as letras maiúsculas \W \D \S
 		// Para pegar todos os caracteres de uma sentença
 			text.match(/[\S\s]/);
 
+
 	// Repetidores
 		// As expressões regulares só fazem o casamento de um único caractere, para fazer a repetição basta utilizar {n,m} com n sendo 'ao menos n vezes' e no méximo 'm' vezes.
 			\d{2,4} // um número entre 2 e quatro caracteres
+
 		// Intervalo aberto, repete ao menos n vezes até o infinito
 			\s{2,}
+
 		// Repete exatamente n vezes
 			\w{2}
+
 		// Caractere opcional ?		
 			\d\d\d? //terceiro caractere opcional
+
 		// Caractere + uma ou infinitas ocorrencias do item anterior
 			s+ // retorna um s ou mais
 			\w+ // faz match com as palavras
+
 		//Caractere * zero ou mais ocorrências
 			\w*
 
