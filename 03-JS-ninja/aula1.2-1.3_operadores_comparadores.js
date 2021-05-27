495d485b8e8736acb763a8f28cce59574cf73eaa
diegosfc100 <diego.sfc100@hotmail.com>
2021-05-27 11:24:13 -0300

    Operadores e Comparadores

---
+++ 03-JS-ninja/aula1.2-1.3_operadores_comparadores.js
@@ -0,0 +1,22 @@
+// OPERADORES E COMPARADORES:
+
+    // var++  incremento, soma + 1
+    // var--  decremento, diminui -1
+
+    var a = 0;
+    var b = 0;
+    console.log(++a); // pré incremento(imprime o 'a' já incrementado) -> 1
+    console.log(b++); // pós incremento(imprime o 'b' e depois incrementa) -> 0
+    console.log(b); // 'b' com incremento -> 1
+
+    
+    //OPERADORES ARITMÉTICOS ABREVIADOS
+        // +=, -=, *=, /=
+        //(var = var + 20) === (var += 20)
+        //(var = var * 20) === (var *= 20)
+
+        // =  atribuição
+        // ==  igual
+        // ===  igual e do mesmo tipo
+        // !=  diferente
+        // !==  diferente, mas do mesmo tipo
