357779df713efc3e225718b41e2efe931bc32392
diegosfc100 <diego.sfc100@hotmail.com>
2021-05-28 17:56:50 -0300

    While

---
+++ 03-JS-ninja/aula6.2_while.js
@@ -0,0 +1,15 @@
+// WHILE
+    // estrutura de loop, fica se repetindo enquanto a condição for verdadeira.
+    // Enquanto [condição] for verdade, faça isso...
+    var counter = 0;
+    while(counter < 10){
+        console.log(counter);
+        counter++;
+    }
+    // contador decrescente
+    var counter2 = 10;
+    while(counter2--){
+        console.log(counter2);
+    }
+
+ 
