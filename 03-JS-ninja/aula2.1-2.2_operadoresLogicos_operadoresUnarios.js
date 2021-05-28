897b91c70cba9c50782481c0a3843fa07f49ed4b
diegosfc100 <diego.sfc100@hotmail.com>
2021-05-28 17:44:15 -0300

    Operadores Lógicos / Operadores Unários

---
+++ 03-JS-ninja/aula2.1-2.2_operadoresLogicos_operadoresUnarios.js
@@ -0,0 +1,27 @@
+// OPERADORES LÓGICOS:
+    var x = 3, y = 4;
+
+    // && Operador "and"
+    x == 3 && y == 4
+
+    // || Operador "or"
+    x == 3 || y == 4 
+
+    // ! Operador "not"
+    x !== 3 // false
+    x !== '3' // true
+
+
+
+// OPERADOR UNARIO
+    // usado para converter em número variáveis que são NaN
+    +'3' // 3
+    +'Diego' // NaN
+
+    // usado para concatenação, sempre que tiver um + somando uma string ele irá retornar uma concatenação
+        'cla' + 'ra' // 'clara'
+        '3' + 3 // '33'
+
+    // Inversor de sinal
+    var a = '5'
+    -a // -5
