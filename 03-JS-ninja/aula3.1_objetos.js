79f4d797b5aab59971a1ce0c175e9574d5f1d3d6
diegosfc100 <diego.sfc100@hotmail.com>
2021-05-28 17:51:10 -0300

    Tipos de dados / Objetos

---
+++ 03-JS-ninja/aula3.1_objetos.js
@@ -0,0 +1,31 @@
+ // Tipos PRIMITIVOS
+    // number = Números
+    // string = Letras e Texto
+    // boolean = true ou false
+    // null e undefined = null é quando não tem valor e undefined é ausencia de valor
+
+
+
+// OBJETOS
+    // Todos os outros que não são primitivos
+    // Um objeto é um conjunto de propriedades
+    var pessoa = {
+        nome: 'Diego Rodrigues',
+        sobrenome: 'de Oliveira',
+        idade: 30,
+        peso: 70,
+        altura: 1.75,
+    }
+
+    var objeto = {
+        propriedade: "valor"            
+    }
+
+    // Acessando objetos
+    pessoa.idade
+    pessoa.nome
+    pessoa.altura
+
+    // Adicionando propriedades
+    pessoa.raça = 'negro';
+    pessoa.time = 'Santos'
